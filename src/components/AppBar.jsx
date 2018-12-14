01820ccc1db9bd194d5616bb5740eca886950767
nonki <thomas@flack.it>
2018-12-14 14:11:35 +0000

    Create basic app bar;

---
+++ src/components/AppBar.jsx
@@ -0,0 +1,47 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import { withStyles  } from '@material-ui/core/styles';
+import MaterialAppBar from '@material-ui/core/AppBar';
+import Toolbar from '@material-ui/core/Toolbar';
+import Typography from '@material-ui/core/Typography';
+import Button from '@material-ui/core/Button';
+import IconButton from '@material-ui/core/IconButton';
+import MenuIcon from '@material-ui/icons/Menu';
+
+const styles = {
+  root: {
+    flexGrow: 1,
+  },
+  grow: {
+    flexGrow: 1,
+  },
+  menuButton: {
+    marginLeft: -12,
+    marginRight: 20,
+  },
+};
+
+export const AppBar = props => {
+  const { classes } = props;
+  return (
+      <div className={classes.root}>
+        <MaterialAppBar position='static'>
+          <Toolbar>
+            <IconButton classname={classes.menuButton} color='inherit' aria-label='Menu'>
+              <MenuIcon />
+            </IconButton>
+            <Typography variant='h6' color='inherit' className={classes.grow}>
+              Kafka PoC
+            </Typography>
+            <Button color='inherit'>Login</Button>
+          </Toolbar>
+        </MaterialAppBar>
+      </div>
+  );
+}
+
+AppBar.propTypes = {
+  classes: PropTypes.object.isRequired,
+};
+
+export default withStyles(styles)(AppBar);
