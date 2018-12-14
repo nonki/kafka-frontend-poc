228f1dac455acc65846b399b92d499ce0e8f1710
nonki <thomas@flack.it>
2018-12-14 15:13:57 +0000

    Add clusters page and route to it

--- src/Router.jsx
+++ src/Router.jsx
@@ -2,10 +2,12 @@ import React from 'react';
 import { Router as ReachRouter } from '@reach/router';
 
 import Home from './views/Home';
+import Clusters from './views/Clusters';
 
 const Router = () => (
   <ReachRouter>
     <Home path="/" />
+    <Clusters path="/clusters" />
   </ReachRouter>
 )
 
--- src/views/Clusters.jsx
+++ src/views/Clusters.jsx
@@ -1,27 +1,26 @@
 import React from 'react';
 import Nav from '../components/Nav';
 
-import Typography from '@material-ui/core/Typography';
 import { withStyles } from '@material-ui/core/styles';
 
 const styles = theme => ({
   title: {
     marginTop: theme.spacing.unit,
     textAlign: 'center',
-  },
+  }
 });
 
-const Home = props => {
+const Clusters = props => {
   const { classes } = props;
 
   return (
     <React.Fragment>
       <Nav />
-      <Typography variant='h1' className={classes.title}>
-        Kafka Management Home Screen
-      </Typography>
+      <div className={classes.title}>
+        Cluster manager page
+      </div>
     </React.Fragment>
   );
 }
 
-export default withStyles(styles)(Home);
+export default withStyles(styles)(Clusters);
